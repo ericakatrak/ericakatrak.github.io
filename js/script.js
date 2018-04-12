58661b7f47fb03432152bae415927d13a0047d5d
Erica Katrak <ericakatrak@users.noreply.github.com>
2018-04-11 19:28:24 -0700

    Update for Dribbble API change

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 // JRIBBBLE
 // Get a list of your shots and display them in the DOM.
 jribbble.shots({token: "32346fbe05ad04f207853e86cc955846ead40d6aece66eec3b1342a19d57be0b"}, function(shotsArray) {
-	var img = (images.hidpi) ? images.hidpi : images.normal;
   document.querySelector(".shotlist").innerHTML = shotsArray.reduce(function(html, shot) {
-    return html + '<li class="shotitem"><a href="'+  shot.html_url + '" target="_blank"><img class="item" src="' + shot.img + '"></a></li>';
+	  var img = (shot.images.hidpi) ? shot.images.hidpi : shot.images.normal;
+    return html + '<li class="shotitem"><a href="'+  shot.html_url + '" target="_blank"><img class="item" src="' + img + '"></a></li>';
   }, "");
 });
 
