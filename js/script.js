1b4601856f095122825af332c178371ffbb5d5fe
Erica Katrak <ericakatrak@users.noreply.github.com>
2018-06-24 19:45:12 -0700

    slight layout shift

--- js/script.js
+++ js/script.js
@@ -13,7 +13,6 @@ function showImages(el) {
     var windowHeight = jQuery( window ).height();
     $(el).each(function(){
         var thisPos = $(this).offset().top;
-
         var topOfWindow = $(window).scrollTop();
         if (topOfWindow + windowHeight - 200 > thisPos ) {
           	$(this).addClass("fadeIn");
