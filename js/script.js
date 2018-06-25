6ab9a70a63290359754d14042ebff9ca091da576
Erica Katrak <ericakatrak@users.noreply.github.com>
2018-06-24 20:04:21 -0700

    slight layout adjustment

--- js/script.js
+++ js/script.js
@@ -7,14 +7,13 @@ jribbble.shots({token: "32346fbe05ad04f207853e86cc955846ead40d6aece66eec3b1342a1
   }, "");
 });
 
-
 // LOADING LOGIC
 function showImages(el) {
     var windowHeight = jQuery( window ).height();
     $(el).each(function(){
         var thisPos = $(this).offset().top;
         var topOfWindow = $(window).scrollTop();
-        if (topOfWindow + windowHeight - 200 > thisPos ) {
+        if (topOfWindow + windowHeight > thisPos ) {
           	$(this).addClass("fadeIn");
         }
     });
