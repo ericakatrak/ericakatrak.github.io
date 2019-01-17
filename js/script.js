cba8f8837dee77c9f49bb3ee14a15fcab6dcf5ff
Erica Katrak <ericakatrak@users.noreply.github.com>
2019-01-16 21:56:32 -0800

    radial gradient

--- js/script.js
+++ js/script.js
@@ -25,3 +25,15 @@ function showImages(el, delay) {
 $(window).scroll(function() {
     showImages('.item', 200);
 });
+
+
+// RADIAL GRADIENT MOVEMENT
+$(document).mousemove(function(event) {
+  windowWidth = $(window).width();
+  windowHeight = $(window).height();
+  
+  mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
+  mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
+  
+  $('.radial-gradient').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #1B1B3D, #1A1F23)');
+});
