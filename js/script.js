c69847487efdda0a27467ae1b2e1fe4f35ce082c
Erica Katrak <ericakatrak@users.noreply.github.com>
2019-02-17 12:10:34 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // JRIBBBLE
 // Get a list of your shots and display them in the DOM.
-jribbble.shots({token: "32346fbe05ad04f207853e86cc955846ead40d6aece66eec3b1342a19d57be0b", per_page: 57}, function(shotsArray) {
+jribbble.shots({token: "32346fbe05ad04f207853e86cc955846ead40d6aece66eec3b1342a19d57be0b", per_page: 60}, function(shotsArray) {
   document.querySelector(".shotlist").innerHTML = shotsArray.reduce(function(html, shot) {
 	  var img = (shot.images.hidpi) ? shot.images.hidpi : shot.images.normal;
     return html + '<li class="shotitem"><a href="'+  shot.html_url + '" target="_blank"><img class="item" src="' + img + '"></a></li>';
